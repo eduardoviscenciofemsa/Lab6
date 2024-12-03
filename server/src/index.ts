6b43820770483273d43148bf6af24f94b34ec93e
eduardoviscenciofemsa <jesus.oliva@digitalfemsa.com>
2024-12-02 18:26:16 -0600

    feat: add validations

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/auth", authRouter);
+app.use("/api/auth", authRouter);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is running on port ${process.env.PORT || 3000}`);
